276cb8b1163017e1f78892dd43a0ec2ddd44936c
Hansith De Silva <de.silva.hansith.lakdinu@iisgalvanimi.edu.it>
2020-12-21 15:58:04 +0000

    punto 2

---
+++ Api-Food/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ItemComponent } from './item/item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    ItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
